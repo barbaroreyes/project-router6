0e0d3ece3654bd2fcc29b8b4a8ba21e04ef82796
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-11-08 17:55:05 -0600

    update-cart

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-
+import React,{useState,useEffect} from 'react'
 import './App.css';
 import Header from './compo/Header';
 import Footer from './compo/Footer';
 import ProductDetails from './compo/ProductDetails';
 import Home from './pages/Home'
-import Products from './pages/Products'
+import Products from './pages/Products';
+import Cart from './pages/Cart'
 import { Routes , Route} from 'react-router-dom'
 const words = ['papa1','papa2','papa3','papa4']
 function App() {
+const [cart,setCart] = useState()
+  const addTocart =(product) => {
+  setCart(product)
+  }
   return (
     <div className="App">
        <Header/>
          <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/products' element={<Products words={words}/>}/>
-            <Route path='/param/:id' element={<ProductDetails words={words}/>}/>
+            <Route path='/' 
+            element={<Home/>}
+            />
+            <Route path='/products' 
+            element={<Products
+             words={words}/>}
+             />
+            <Route path='/cart'
+             element={<Cart cart={cart} 
+             words={words}
+             />}
+             />
+            <Route path='/param/:id'
+             element={
+            <ProductDetails 
+            words={words}
+            addTocart={addTocart}
+            />}
+            />
           </Routes>
         
        <Footer/>
--- src/compo/ProductDetails.js
+++ src/compo/ProductDetails.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import {useParams} from 'react-router-dom'
+import {useParams,useNavigate} from 'react-router-dom'
 import style from 'styled-components';
 
 const Container = style.div`
 border:1px solid;
 display: flex;
 flex-direction: column;
+width:100px;
+heigth:100px;
 `
 const Param = (props) => {
- 
+    const navigate= useNavigate()
     const param = useParams()
     console.log(param,'params')
   return (
-    <Container>
+    <Container onClick={()=>navigate('/cart')}>
       {props.words[param.id]}
     </Container>
   )
